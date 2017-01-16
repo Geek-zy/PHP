d1d90f03bb4bb4ed136c9cc546528ec5bb1606c2
Geek-zy <zy251021@163.com>
2017-01-16 15:36:22 +0800

    PHP获取时间

---
+++ Date_Time/Date_Time.php
@@ -0,0 +1,30 @@
+<?php
+//设置时区(中国标准时间)
+date_default_timezone_set('PRC');
+
+/*时间格式的时间
+echo date('Y-m-d H:i:s');
+时间戳格式的时间
+echo time();
+*/
+
+$Date_Time = date('s') . "\n";
+
+if ($Date_Time >= 0 && $Date_Time <= 10)
+	echo $Date_Time . "0-10 秒段\n";
+
+else if ($Date_Time >= 10 && $Date_Time <= 20) 
+	echo $Date_Time . "10-20 秒段\n";
+
+else if ($Date_Time >= 20 && $Date_Time <= 30) 
+	echo $Date_Time . "20-30 秒段\n";
+
+else if ($Date_Time >= 30 && $Date_Time <= 40) 
+	echo $Date_Time . "30-40 秒段\n";
+
+else if ($Date_Time >= 40 && $Date_Time <= 50) 
+	echo $Date_Time . "40-50 秒段\n";
+
+else 
+	echo $Date_Time . "60-0 秒段\n";
+?>
